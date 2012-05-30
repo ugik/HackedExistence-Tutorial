ffc216a482aa668e4c467b96b25b2ab6ba088a1e
ugik <georgek@gmail.com>
2012-05-29 23:33:10 -0400

    Chapter 3 - Views and Templates

--- beer/views.py
+++ beer/views.py
@@ -1,1 +1,19 @@
-# Create your views here.
+from django.shortcuts import render_to_response
+from django.template import RequestContext
+from beer.models import Beer, Brewery
+
+def BeersAll(request):
+    beers = Beer.objects.all().order_by('name')
+    context = {'beers': beers}
+    return render_to_response('beersall.html', context, context_instance=RequestContext(request))
+    
+def SpecificBeer(request, beerslug):
+    beer = Beer.objects.get(slug=beerslug)
+    context = {'beer': beer}
+    return render_to_response('singlebeer.html', context, context_instance=RequestContext(request))
+    
+def SpecificBrewery(request, breweryslug):
+    brewery = Brewery.objects.get(slug=breweryslug)
+    beers = Beer.objects.filter(brewery=brewery)
+    context = {'beers': beers}
+    return render_to_response('singlebrewery.html', context, context_instance=RequestContext(request))
--- video1/urls.py
+++ video1/urls.py
@@ -1,7 +1,13 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from django.views.generic.simple import direct_to_template
 admin.autodiscover()
 
 urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
+    (r'^$', direct_to_template, {'template': 'index.html'}),
+    (r'^beers/$', 'beer.views.BeersAll'),
+    (r'^beers/(?P<beerslug>.*)/$', 'beer.views.SpecificBeer'),
+    (r'^brewerys/(?P<breweryslug>.*)/$', 'beer.views.SpecificBrewery'),
 )
+
